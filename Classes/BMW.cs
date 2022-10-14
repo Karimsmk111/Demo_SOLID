cac1e9738411248e5c29c624ac9dd8b105b32dc7
WinWire <WinWire@DESKTOP-G5NE1TV>
2022-10-14 15:08:09 +0530

    Add project files.

---
+++ Classes/BMW.cs
@@ -0,0 +1,23 @@
+﻿using Demo_SOLID.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_SOLID.Classes
+{
+    internal class BMW : Cars
+    {
+        public string GetCar()
+        {
+            
+            return "Display";
+        }
+
+        public double GetCost()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
